4bbe3329f250e4017683b2f3c3c3dd4aac6eca69
oo-ng <122175989+oo-ng@users.noreply.github.com>
2023-08-15 11:41:03 -0400

    Linked all pages together

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,19 +1,21 @@
 import { NavLink } from "react-router-dom"
 import styles from "./PageNav.module.css"
+import Logo from "./Logo"
 export const PageNav =()=>{
 
 
     return (
         <nav className={styles.nav}>
+            <Logo/>
             <ul >
-                <li>
-                    <NavLink to="/">Home</NavLink>
-                </li>
                 <li>
                     <NavLink to="/product">Products</NavLink>
                 </li>
                 <li>
                     <NavLink to="/pricing">Pricing</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/login">Login</NavLink>
                 </li>
             </ul>
         </nav>
